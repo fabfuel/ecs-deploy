7888ca714f26288897a675f5848e058a19df682a
Fabian Fülling <git@fabfuel.de>
2016-03-13 10:15:00 +0100

    Implement actions "deploy" and "scale" and create test infrastructure

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 import pytest
 from click.testing import CliRunner
+
 from ecs_deploy import cli
 
 
@@ -12,18 +13,3 @@ def test_cli(runner):
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
     assert not result.exception
-    assert result.output.strip() == 'Hello, world.'
-
-
-def test_cli_with_option(runner):
-    result = runner.invoke(cli.main, ['--as-cowboy'])
-    assert not result.exception
-    assert result.exit_code == 0
-    assert result.output.strip() == 'Howdy, world.'
-
-
-def test_cli_with_arg(runner):
-    result = runner.invoke(cli.main, ['Fabian'])
-    assert result.exit_code == 0
-    assert not result.exception
-    assert result.output.strip() == 'Hello, Fabian.'
