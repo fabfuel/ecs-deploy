29ad754defe2b9f9c8e9f7ed7f240439bd053842
igorkotof <igor.kotov@hazelops.com>
2021-05-05 17:50:25 +0300

    get_task_stop_reason - added try\except

--- ecs_deploy/ecs.py
+++ ecs_deploy/ecs.py
@@ -731,18 +731,21 @@ class EcsAction(object):
         return service.desired_count == running_count
 
     def get_task_stop_reason(self, service):
-        stopped_tasks = self._client.list_stopped_tasks(
-            cluster_name=service.cluster,
-            service_name=service.name
-        )
-        task_arns=stopped_tasks[u'taskArns']
-        tasks_details = self._client.describe_tasks(
-            cluster_name=self._cluster_name,
-            task_arns=task_arns
-        )
-        for task in tasks_details[u'tasks']:
-            task_stopped_reason = task[u'stoppedReason']
-        return task_stopped_reason
+        try:
+            stopped_tasks = self._client.list_stopped_tasks(
+                cluster_name=service.cluster,
+                service_name=service.name
+            )
+            task_arns=stopped_tasks[u'taskArns']
+            tasks_details = self._client.describe_tasks(
+                cluster_name=self._cluster_name,
+                task_arns=task_arns
+            )
+            for task in tasks_details[u'tasks']:
+                task_stopped_reason = task[u'stoppedReason']
+            return task_stopped_reason
+        except ClientError:
+            return task_stopped_reason = u'[Warning] Unknown stopped reason'
 
     def get_running_tasks_count(self, service, task_arns):
         running_count = 0
