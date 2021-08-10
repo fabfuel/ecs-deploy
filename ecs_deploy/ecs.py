a62bab1cf10a57c9ee7d529b86915061e5e0a0a3
Michael Mayer <mjmayer@ucdavis.edu>
2021-08-10 16:37:59 -0700

    Refactor parsing of s3 env files to support python 2.7

--- ecs_deploy/ecs.py
+++ ecs_deploy/ecs.py
@@ -654,23 +654,25 @@ class EcsTaskDefinition(object):
         self._diff.append(diff)
         
         container[u'dockerLabels'] = merged.copy()
-    def set_s3_env_file(self, s3_env_files_list, exclusive=False):
-        s3_files = defaultdict(list)
-        if len(s3_env_files_list) > 0:
-            if isinstance(s3_env_files_list[0], str):
-                s3_files[s3_env_files_list[0]] = {s3_env_files_list[1]}
-            if isinstance(s3_env_files_list[0], tuple):
-                for s3_file in s3_env_files_list:
-                    if s3_files[s3_file[0]]:
-                        s3_files[s3_file[0]].add(s3_file[1])
+    def set_s3_env_file(self, s3_env_files, exclusive=False):
+        # environmentFiles in task definition https://docs.aws.amazon.com/AmazonECS/latest/developerguide/taskdef-envfiles.html
+        s3_files_by_container = defaultdict(list)
+        if s3_env_files:
+            multiple_s3_env_files = any(isinstance(i, tuple) for i in s3_env_files)
+            if not multiple_s3_env_files:
+                s3_files_by_container[s3_env_files[0]] = {s3_env_files[1]}
+            if multiple_s3_env_files:
+                for s3_file in s3_env_files:
+                    if s3_files_by_container[s3_file[0]]:
+                        s3_files_by_container[s3_file[0]].add(s3_file[1])
                         break
-                    s3_files[s3_file[0]] = {s3_file[1]}
+                    s3_files_by_container[s3_file[0]] = {s3_file[1]}
 
         for container in self.containers:
-            if container[u'name'] in s3_files:
+            if container[u'name'] in s3_files_by_container:
                 self.apply_s3_env_file(
                     container=container,
-                    new_s3_env_file=s3_files[container[u'name']],
+                    new_s3_env_file=s3_files_by_container[container[u'name']],
                     exclusive=exclusive
                 )
             elif exclusive is True:
